eea44cfb4d124f693d2d83949a71165b9949f995
Caio Fernandes <caiovr13@gmail.com>
2020-07-15 23:53:51 -0300

    Alteração no main

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ import hashlib
 from Sensor import Sensor
 
 def main():
-    frequency = 125
-    seconds = 5
-    numberOfBlocks = 20
-    filterMin = -15
-    filterMax = 15
-
+    
     data = pd.read_csv("p000020-2183-04-28-17-471.csv", header=None)
     
     # Extraindo as linhas do arquivo, pegando a segunda coluna (II - ECG)
     data = data[1:10000][1]
     data = np.array(data).astype(int)
 
-    sensorTransmitter = Sensor(frequency, seconds, numberOfBlocks, filterMin, filterMax)
-    sensorTransmitter.extractFeats(data)
+    EKGPROTOCOL(data)
+    
 
+def EKGPROTOCOL(data):
+
+    frequency = 125
+    seconds = 5
+    numberOfBlocks = 20
+    filterMin = -15
+    filterMax = 15
+
+    sensorTransmitter = Sensor(frequency, seconds, numberOfBlocks, filterMin, filterMax)
     sensorReceiver = Sensor(frequency, seconds, numberOfBlocks, filterMin, filterMax)
+
+    sensorTransmitter.extractFeats(data)
     sensorReceiver.extractFeats(data)
 
     sensorTransmitter.receiveCommitmentMessage(sensorReceiver.getCommitmentMessage())
@@ -32,5 +38,4 @@ def main():
 
     sensorTransmitter.receiveDecommitmentMessage(sensorReceiver.getDecommitmentMessage())
     sensorReceiver.receiveDecommitmentMessage(sensorTransmitter.getDecommitmentMessage())
-
 main()
